80a7f116fdfaeddee4bd38625659f2a1a7c155a7
Sha Jie <148933639+Kelvin-Serendipity@users.noreply.github.com>
2024-02-08 18:46:33 +0800

    1

---
+++ assets/js/xxx.js
@@ -0,0 +1,9 @@
+//版本号统一修改
+document.addEventListener('DOMContentLoaded', function () {
+    var updateElements = document.querySelectorAll('.version-update'); // 选择带有类名 'version-update' 的元素
+    if (updateElements.length > 0) {
+        updateElements.forEach(function (element) {
+            element.textContent = '版本号：1.1.2'; // 修改内容
+        });
+    }
+});
